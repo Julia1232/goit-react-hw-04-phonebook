2704e3bc21793d1685faad9f1d2647cc0bbdc167
Julia Petrenko <juliapetrenko02.07.1999@gmai.com>
2023-02-27 11:20:14 +0200

    firt

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(
+  <>
     <App />
-  </React.StrictMode>
+  </>,
+  document.getElementById("root")
 );
