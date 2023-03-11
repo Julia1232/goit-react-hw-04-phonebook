392b99e91713af7450d4ec06da106fb42880f94a
Julia Petrenko <juliapetrenko02.07.1999@gmai.com>
2023-03-11 16:18:54 +0200

    firsdt

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,10 +11,28 @@ export default class App extends Component {
       { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
       { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
       { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" }
     ],
-    filter: "",
+    filter: ""
   };
+
+  componentDidMount() {
+ 
+    const state = localStorage.getItem('contacts')
+    const parsedContacts = JSON.parse(state);
+
+    if ( parsedContacts) {
+      this.setState({contacts: parsedContacts})
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log(prevState)
+    console.log(this.state)
+  if (this.state.contacts !== prevState.contacts) {
+    localStorage.setItem('contacts',JSON.stringify(this.state.contacts))
+  }
+}
 
   addContact = (task) => {
     const searchSameName = this.state.contacts
@@ -35,6 +53,9 @@ export default class App extends Component {
       this.setState((prevState) => ({
         contacts: [...prevState.contacts, contact],
       }));
+      
+    localStorage.setItem('todos', JSON.stringify(contact))
+      
     }
   };
 
